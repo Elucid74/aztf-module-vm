c64830a3a93899abaa024bef6135a0b58f43678f
Calvin Kim <hyuk@microsoft.com>
2019-12-29 21:19:46 +0900

    bugfix

--- variables.tf
+++ variables.tf
@@ -113,21 +113,22 @@ variable "enable_dependency_agent" {
 
 variable "load_balancer_param" {
   description = "load balancer parameters"
-  type = list(object({
+  type = object({
     sku             = string
     probe_protocol  = string
     probe_port      = number
     probe_interval  = number
     probe_num       = number
-  }))
-  default = [
-    {
+  })
+  default = null
+  /* example
+  default = {
       sku             = "basic"
       probe_protocol  = "Tcp"
       probe_port      = 22
       probe_interval  = 5
       probe_num       = 2
-    }
-  ]
+  }
+  */
 }
 
