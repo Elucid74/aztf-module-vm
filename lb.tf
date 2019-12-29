ff56d97648f62065345e6ab9386aef6739880d6d
Calvin Kim <hyuk@microsoft.com>
2019-12-29 21:37:21 +0900

    bug fix

--- lb.tf
+++ lb.tf
@@ -30,8 +30,6 @@ resource "azurerm_lb_backend_address_pool" "lb" {
     resource_group_name             = azurerm_lb.lb.0.resource_group_name
     loadbalancer_id                 = azurerm_lb.lb.0.id
     name                            = "backendpool"
-
-    depends_on    = ["null_resource.dependency_getter"]
 }
 
 resource "azurerm_lb_rule" "https" {
